a7824ff6851f0e1de0ca21f347e074d603a692dc
aakash1007007 <aakashravuri07@gmail.com>
2026-04-08 15:23:37 +0530

    UC3 - Track Unique Bogie IDs

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,10 +1,23 @@
+import java.util.HashSet;
+
 public class TrainConsistManagementApp {
+
     public static void main(String[] args) {
 
         System.out.println("====================================");
-        System.out.println("   Welcome to Book My Stay App");
-        System.out.println("   Hotel Booking System v1.0");
+        System.out.println("   Train Consist Management App");
         System.out.println("====================================");
 
+        HashSet<String> bogieIDs = new HashSet<>();
+
+        bogieIDs.add("BG101");
+        bogieIDs.add("BG102");
+        bogieIDs.add("BG103");
+        bogieIDs.add("BG101"); // duplicate
+        bogieIDs.add("BG102"); // duplicate
+
+        System.out.println("\nUnique Bogie IDs in Train:");
+        System.out.println(bogieIDs);
     }
+
 }
